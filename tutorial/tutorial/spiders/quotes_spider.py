af942fb0ccd4a8c2af3270d0f8392a070a8edf6d
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-28 09:40:46 +0200

    Update quotes_spider.py

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -10,14 +10,9 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        """
-        a method that will be called to handle the response downloaded for each of the requests made. 
-        The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it.
-        The parse() method usually parses the response, extracting the scraped data as dicts and also finding new URLs to follow
-        and creating new requests (Request) from them.
-        """
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as file:
-            file.write(response.body)
-        self.log(f'Saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall()
+            }
